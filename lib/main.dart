590ea38df7c952acfb41535a96bb224c805294a1
Seif-Elnawawy <elnawawyseif@gmail.com>
2024-11-30 17:23:11 +0200

    register adapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app2/constants.dart';
+import 'package:notes_app2/models/note_model.dart';
 import 'package:notes_app2/view/edit_note_view.dart';
 import 'package:notes_app2/view/notes_view.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -8,6 +9,7 @@ void main() async {
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
 
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(const NotesApp());
 }
 
