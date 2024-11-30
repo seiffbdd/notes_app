98b3c4451f4d46be17f558ef4239268d838f8bdc
Seif-Elnawawy <elnawawyseif@gmail.com>
2024-11-30 17:19:47 +0200

    add notes cubit

---
+++ lib/cubits/cubit/add_note_state.dart
@@ -0,0 +1,16 @@
+part of 'add_note_cubit.dart';
+
+@immutable
+sealed class AddNoteState {}
+
+final class AddNoteInitial extends AddNoteState {}
+
+final class AddNoteLoading extends AddNoteState {}
+
+final class AddNoteSuccess extends AddNoteState {}
+
+final class AddNoteFairlure extends AddNoteState {
+  final String errMessage;
+
+  AddNoteFairlure({required this.errMessage});
+}
